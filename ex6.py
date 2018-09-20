cde6a0075b354fab82d85757f70b71c564aa1078
hajaking <953258481@qq.com>
2018-09-20 17:49:59 +0800

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,18 +1,30 @@
-#only find by name in fantasy way
-from pathlib import Path
-import  argparse
+#by name or by type
+# if  we type  ex6.py -t f   in the console  it will be wrong
+from  pathlib import  Path
+import argparse
+import sys
 
 parser=argparse.ArgumentParser()
-
-parser.add_argument('start',type=str)
+parser.add_argument('start',type=str,nargs=1)
 parser.add_argument('-n','--name',type=str)
 parser.add_argument('-t','--type',type=str)
 
 args=parser.parse_args()
 print(args)
 
-#断点
-start_path=Path(args.start[0])#address
-
-for f in start_path.rglob(args.name):#name
-	print(f)
+start_path=Path(args.start[0])
+if args.name:
+	for f in start_path.rglob(args.name):
+		print(f)
+elif args.type:
+	for f in start_path.rglob(args.name):
+		if args.type== "d" and f.is_dir():
+			print(f)
+		elif args.type== "f" and f.is_file():
+			print(f)
+		else:
+			print("unknown type：{args.type}")
+			sys.exit(1)
+else:
+	print("please print -n or -t")
+	sys.exit(1)
