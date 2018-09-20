6b95c0c88791dad462ca6af0661874ccbc11988f
hajaking <953258481@qq.com>
2018-09-20 17:51:41 +0800

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,22 +1,35 @@
-#we can use f !!
+#put all the things into  def()
 from  pathlib import  Path
 import argparse
 import sys
 
-parser=argparse.ArgumentParser()
-parser.add_argument('start',type=str,nargs=1)
-parser.add_argument('-n','--name',type=str)
-parser.add_argument('-t','--type',type=str)
 
-args=parser.parse_args()
-print(args)
+def  parse_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('start',type=str,nargs=1)
+    parser.add_argument('-n','--name',type=str)
+    parser.add_argument('-t','--type',type=str)
 
-start_path=Path(args.start[0])
-if args.name:
+    return parser.parse_args()
+
+
+def find_file(args):
+    start_path=Path(args.start[0])
+    if args.name and not args.type:
+        name_find(start_path,args)
+    elif args.type:
+        type_find(start_path,args)
+    else:
+        print("please print -n or -t")
+        sys.exit(1)
+
+
+
+def name_find(start_path,args):
     for f in start_path.rglob(args.name):
         print(f)
 
-elif args.type:
+def type_find(start_path,args):
     if args.type not in ['d','f']:
         print("unknown type：{args.type}")
         sys.exit(1)
@@ -26,6 +39,4 @@ elif args.type:
         elif args.type== "f" and f.is_file():
             print(f)
 
-else:
-    print("please print -n or -t")
-    sys.exit(1)
+find_file(parse_args())
